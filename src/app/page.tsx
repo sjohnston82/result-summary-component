aa0ffad89fcb1643a1de2cbe1d8a11b546359cb7
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2023-08-19 15:52:17 -0400

    adding container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import Summary from "@/components/Summary";
 export default function Home() {
   return (
     <main className="font-bold bg-white font-cfont lg:flex lg:items-center lg:justify-center lg:h-[100dvh] ">
-      <div className="lg:flex lg:w-1/2 lg:h-1/2 lg:items-center lg:justify-center lg:shadow-lightlavender lg:shadow-2xl lg:rounded-3xl">
-        <ScoreContainer />
-        <Summary />
+      <div className="lg:w-[1440px] lg:h-[900px] lg:grid lg:place-items-center">
+        <div className="lg:flex lg:w-1/2 lg:h-1/2 lg:items-center lg:justify-center lg:shadow-lightlavender lg:shadow-2xl lg:rounded-3xl">
+          <ScoreContainer />
+          <Summary />
+        </div>
       </div>
     </main>
   );
--- src/components/ScoreContainer.tsx
+++ src/components/ScoreContainer.tsx
@@ -5,7 +5,7 @@ const ScoreContainer = () => {
   return (
     <div
       className="h-[50dvh] flex flex-col justify-center items-center
-     bg-gradient-to-b lg:h-full lg:rounded-3xl lg:w-[50%] lg:mr-auto from-lightslateblue to-lightroyalblue w-full rounded-b-[50px]"
+     bg-gradient-to-b lg:h-full lg:rounded-3xl lg:w-[50%] lg:mr-auto from-lightslateblue via-lightroyalblue to-lightroyalblue w-full rounded-b-[50px]"
     >
       <ScoreInfo />
     </div>
